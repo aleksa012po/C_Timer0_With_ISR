44107f51d3957059cc45cc559e4a5157cf04ab2b
Aleksandar <102791994+aleksa012po@users.noreply.github.com>
2023-02-05 22:39:45 +0100

    Update main.c

--- C_Arduino_Timer0_With_ISR/main.c
+++ C_Arduino_Timer0_With_ISR/main.c
@@ -2,14 +2,14 @@
  * C_Arduino_Asembler_Timers.c
  *
  * Created: 24.11.2022. 08:35:51
- * Author : user
+ * Author : Aleksandar Bogdanovic
  */ 
 
 // Arduino Asembler, Tajmeri. Proci tajmere koriscenjem interapt-a
 // 8 bit Timer - 256 clock cycles
-// TCCR0A – Timer/Counter Control Register A
-// TCCR0B – Timer/Counter Control Register B
-// TIMSK0 – Timer/Counter Interrupt Mask Register
+// TCCR0A â€“ Timer/Counter Control Register A
+// TCCR0B â€“ Timer/Counter Control Register B
+// TIMSK0 â€“ Timer/Counter Interrupt Mask Register
 // Real time 0.01 sek, total timer ticks 156.25, freq 100Hz, delay 500ms, clk 16MHz
 
 #define F_CPU 16000000							// 16MHz
